7642fd6a65b545afcd129f5c66b82d237d4e0277
rleme <rodrigocleme@gmail.com>
2021-11-01 11:29:47 -0300

    general

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ Website: https://www.allphptricks.com
 
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("apidb1979.mysql.database.azure.com","api@apidb1979t","Bi@06gata","allphptricks");
+$con = mysqli_connect("apidb1979.mysql.database.azure.com","api","Bi@06gata","allphptricks");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
