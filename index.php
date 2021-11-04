9917130c7e7b7d974058cc3221a4e9f2e3d80e6c
rleme <rodrigocleme@gmail.com>
2021-11-04 17:48:34 -0300

    Simulate issue.

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (isset($_POST['order_id']) && $_POST['order_id']!="") {
 	$response = curl_exec($client);
 	
 	$result = json_decode($response);
-	
+	$a > $b;
 	echo "<table>";
 	echo "<tr><td>CPF:</td><td>$result->order_id</td></tr>";
 	echo "<tr><td>Valor:</td><td>$result->amount</td></tr>";
