e8c2485e400f74617847b64ab43c5868b6022408
rleme <rodrigocleme@gmail.com>
2021-11-11 17:26:56 -0300

    Add secret dbuser dbpass

--- db.php
+++ db.php
@@ -1,5 +1,9 @@
 <?php
-$con = mysqli_connect("res-php-api1979.mysql.database.azure.com","api","Bi@06gata","allphptricks");
+
+$dbuser = getenv("dbuser");
+$dbpass = getenv("dbpass");
+
+$con = mysqli_connect("res-php-api1979.mysql.database.azure.com",$dbuser,$dbpass,"allphptricks");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		die();
